335f0295661b75d7b703026caf1388033a374ea5
sikijoy <1085609552@qq.com>
2022-01-11 17:05:55 +0800

    第一次提交

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<!-- template 只能向外部暴露一个根标签 -->
+    <div>
+        <div class="home">home</div>
+        <router-link to="/list" class="home">列表页</router-link>
+    </div>
+
+</template>
+<script>
+    export default
+    {
+        name:'Home'
+    }
+</script>
+<style>
+.home{
+    font-size: 50px;
+    
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        List
+    </div>
+</template>
+<script>
+    export default
+    {
+        name:'List'
+    }
+</script>
+<style>
+
+</style>
